560065b5f02654a4d97d28119928118b23309b86
marskr <marskr@opoczta.pl>
2017-12-30 14:17:00 +0100

    board screening implemented

--- algorithm.hs
+++ algorithm.hs
@@ -1,12 +1,6 @@
 import Data.List
 
-_zip :: [a] -> [b] -> [(a,b)]
-_zip _ [] = []
-_zip [] _ = []
-_zip (x:xs) (y:ys) = (x,y) : _zip xs ys
-
 --maximum (map fst [(1,2),(3,4)])
-
 {- --non-used old method
 _changeAt :: (Eq i, Num i) => i -> [Char] -> [Char]
 _changeAt _ [] = []
@@ -15,34 +9,53 @@ _changeAt n (x:xs)
                   | otherwise = x : _changeAt (n-1) xs
 -}
 
+-- check if in table exist provided item
 _checkIfExist :: [Int] -> Int -> Bool
 _checkIfExist [] _ = False
 _checkIfExist (x:xs) n 
                        | x == n = True
 					   | otherwise = _checkIfExist xs n
 
-
-		-- [Lista miejsc do zmiany] -> maksymalny elem na liscie ->
-		-- [Lista charów zmienianych] -> wynik (board) 
+	-- [Lista miejsc do zmiany] -> maksymalny elem na liscie ->
+	-- [Lista charów zmienianych] -> wynik (board) 
+-- placing houses on provided char list of elements
 _changeAtBasic :: [Int] -> Int -> [Char] -> [Char]
 _changeAtBasic _ _ [] = [] 
 _changeAtBasic xs n (y:ys)
 				  | _checkIfExist xs n == True = 'h' : _changeAtBasic xs (n-1) ys
 				  | otherwise = y : _changeAtBasic xs (n-1) ys
 
-
+-- processing provided tuple into an index of table number
 _processNo :: (Int,Int) -> Int
-_processNo (n,t) = 6 * n  + t
+_processNo (n,t) = 6 * n + t
 
-_board :: Int -> char -> [char]
+-- board creation 
+_board :: Int -> Char -> [Char]
 _board 0 _ = []
 _board n x = x : _board (n-1) x
 
-
+-- ELEMENTS PROVIDING POSIBILITY TO SHOW HOUSES BOARD
+-- show board by index: 
+-- _showBoardByIndex index iterator array
+_showBoardByIndex :: Int -> Int -> [Char] -> [Char]
+_showBoardByIndex _ _ [] = []
+_showBoardByIndex index iterator (x:xs) 
+				  | index == iterator = (_getList 6 (x:xs))
+				  |	otherwise = _showBoardByIndex index (iterator + 1) xs
+				  
+-- get 'length' elements of the list
+_getList :: Int -> [Char] -> [Char]
+_getList _ [] = []
+_getList length (x:xs) 
+				  | (length > 0) = x : _getList (length - 1) xs
+				  | otherwise = _getList (length - 1) xs
+			
 
 main = do
-	--putStrLn (_changeAt (_processNo (1,2)) (_board 36 'x'))
-	
-	putStrLn (reverse (_changeAtBasic (map _processNo [(0,1),(3,2),(3,4),(4,0),(4,4),(5,2),(5,5)]) 35 (_board 36 'x')))
-	
+	putStrLn (_showBoardByIndex 0 0 (reverse (_changeAtBasic (map _processNo [(0,1),(3,2),(3,4),(4,0),(4,4),(5,2),(5,5)]) 35 (_board 36 'x'))))
+	putStrLn (_showBoardByIndex 6 0 (reverse (_changeAtBasic (map _processNo [(0,1),(3,2),(3,4),(4,0),(4,4),(5,2),(5,5)]) 35 (_board 36 'x'))))
+	putStrLn (_showBoardByIndex 12 0 (reverse (_changeAtBasic (map _processNo [(0,1),(3,2),(3,4),(4,0),(4,4),(5,2),(5,5)]) 35 (_board 36 'x'))))
+	putStrLn (_showBoardByIndex 18 0 (reverse (_changeAtBasic (map _processNo [(0,1),(3,2),(3,4),(4,0),(4,4),(5,2),(5,5)]) 35 (_board 36 'x'))))
+	putStrLn (_showBoardByIndex 24 0 (reverse (_changeAtBasic (map _processNo [(0,1),(3,2),(3,4),(4,0),(4,4),(5,2),(5,5)]) 35 (_board 36 'x'))))
+	putStrLn (_showBoardByIndex 30 0 (reverse (_changeAtBasic (map _processNo [(0,1),(3,2),(3,4),(4,0),(4,4),(5,2),(5,5)]) 35 (_board 36 'x'))))
 	
